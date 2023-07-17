233cdc707044b93878796f1efe72c669513f2a19
ShobaneTK <125491523+ShobaneTK@users.noreply.github.com>
2023-07-18 00:06:25 +0100

    Add files via upload

---
+++ MyFirstFrame.java
@@ -0,0 +1,21 @@
+
+package za.ac.tut.ui;
+
+import java.awt.Color;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author Asus
+ */
+public class MyFirstFrame extends JFrame{
+    public MyFirstFrame(){
+        setTitle("My First GUI");
+        setSize(200, 250);
+        setDefaultLookAndFeelDecorated(true);
+        setResizable(true);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setForeground(Color.BLUE);
+        setVisible(true);
+    }
+}
